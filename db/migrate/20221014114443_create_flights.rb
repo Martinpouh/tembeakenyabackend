4a132ca5faf6e478f31de2ace195665a6d11575b
Martinpouh <martinchege2015@gmail.com>
2022-10-14 14:51:24 +0300

    Add code to flight migration

--- db/migrate/20221014114443_create_flights.rb
+++ db/migrate/20221014114443_create_flights.rb
@@ -1,7 +1,14 @@
 class CreateFlights < ActiveRecord::Migration[7.0]
   def change
     create_table :flights do |t|
-
+      t.string :name 
+      t.string :description
+      t.string :creator
+      t.string :location
+      t.string :start_date
+      t.string :end_date
+      t.string :image
+      t.integer :budget
       t.timestamps
     end
   end
