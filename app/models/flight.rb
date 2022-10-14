9d7560dac8de6a8e224e57d97c618ba4ca19514d
Martinpouh <martinchege2015@gmail.com>
2022-10-14 14:49:13 +0300

    Add code to flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,7 @@
 class Flight < ApplicationRecord
+    has_many :schedules, dependent: :destroy
+    has_many :passenger_flights, dependent: :destroy
+    has_many :passengers, through: :passenger_flights
+
+    validates :name, :description, :creator, :location, :start_date, :end_date, :image, :budget, presence: true
 end
